53b2d041a447484c68be522c1c7c5784191093aa
Levy-Naibei <levynaibei2@gmail.com>
2020-06-05 12:04:16 +0300

    codacy

--- src/axiosConfigs.js
+++ src/axiosConfigs.js
@@ -2,67 +2,56 @@ import axios from 'axios';
 
 // consume login endpoint
 export const LoginUser = (email, password) => {
-         
-    let data = {
-            email: email,
-            password: password
-        }
+  const data = {
+    email: email,
+    password: password
+  }
+  
+  axios.post('http://localhost:8000/auth/v1/login/', data)
+    .then(res => {  
+    const token = res.data.token;
+    const userType = res.data.user.User_type;
+    // let userName = res.data.username;
     
-    axios.post('http://localhost:8000/auth/v1/login/', data)
-        .then(res => {
-            
-        let token = res.data.token;
-        let userType = res.data.user.User_type;
-        // let userName = res.data.username;
-        
-        localStorage.setItem("isLoggedIn", token);
-        localStorage.setItem("userType", userType);
-        // localStorage.setItem("userName", userName);
+    localStorage.setItem('isLoggedIn', token);
+    localStorage.setItem('userType', userType);
+    // localStorage.setItem("userName", userName);
 
-        if (userType === 'farmer'){
-            window.location.href = '/farmers';
-        }
-    
-        else if (userType === 'customer'){
-            window.location.href = '/customers';
-        }
-    })
-    //   .catch(err => console.log(err))
+    if (userType === 'farmer') {
+      window.location.href = '/farmers';
+    }
+
+    else if (userType === 'customer') {
+      window.location.href = '/customers';
+    }
+})
+//   .catch(err => console.log(err))
 }
 
 // consume register enpoint
-export const SignUp = (
-    first_name, surname, 
-    username, email,
-    password, location, 
-    phone_number, id_number,
-    User_type, image
-    ) => {
+export const SignUp = (first_name, surname, username, email, password, location, phone_number, id_number,User_type, image) => {
+  const data = {
+    first_name: first_name,
+    surname: surname,
+    username: username,
+    email: email,
+    password: password,
+    location: location,
+    phone_number: phone_number,
+    id_number: id_number,
+    User_type: User_type,
+    image: image
+  }
 
-    let data = {
-            first_name: first_name,
-            surname: surname,
-            username: username,
-            email: email,
-            password: password,
-            location: location,
-            phone_number: phone_number,
-            id_number: id_number,
-            User_type: User_type,
-            image: image
-        }
+  console.log(data)
 
-    console.log(data)
+  const url = 'http://localhost:8000/auth/v1/register/';
 
-    let url = 'http://localhost:8000/auth/v1/register/';
-
-    axios.post(url, JSON.stringify(data))
-    .then(res => {
-            
-            console.log(res.data.user);
-        
-        window.location.href = '/login';
-    })
-    // .catch(err => console.log(err))
+  axios.post(url, JSON.stringify(data))
+  .then(res => {   
+    console.log(res.data.user);
+    window.location.href = '/login';
+  })
+  // .catch(err => console.log(err))
 
 }
